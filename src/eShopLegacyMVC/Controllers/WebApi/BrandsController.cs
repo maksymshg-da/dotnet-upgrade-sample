3767d419010c91a71c12165904b7745c15558d63
Maksym Shekhovets <maksym.shekhovets@dataart.com>
2025-09-04 08:50:22 +0300

    * 'Line 7: Remove the using directive for System.Runtime.Remoting.Messaging, as this namespace does not exist in .NET 8.0 and is not needed in this file.' in file 'src\eShopLegacyMVC\Controllers\WebApi\BrandsController.cs'

--- src/eShopLegacyMVC/Controllers/WebApi/BrandsController.cs
+++ src/eShopLegacyMVC/Controllers/WebApi/BrandsController.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Runtime.Remoting.Messaging;
 using System.Web.Http;
 
 namespace eShopLegacyMVC.Controllers.WebApi
